b6900c3cc1617c500118b0f3477f3e42c353b8b3
donnettew <william.donnette@etu.umontpellier.fr>
2021-12-16 09:24:14 +0100

    [GIT] - Init

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
 const express = require('express')
 const app = express()
 const PORT = process.env.PORT || 5000 // this is very important
+var request = require("request");
+
+var options = {
+    method: 'GET',
+    url: 'https://tpheroku-9839.restdb.io/rest/recettes',
+    headers:
+    {
+        'cache-control': 'no-cache',
+        'x-apikey': '5ffba6c0c4842c6df4996e561c870bcc37da8'
+    }
+};
+
+
 
 app.get('/', function (req, res) {
-  res.send('Hello World!')
+    request(options, function (error, response, body) {
+        if (error) throw new Error(error);
+        res.send(body);
+    });
 })
 
 app.listen(PORT, function () {
-  console.log('Example app listening on port ' + PORT)
+    console.log('Example app listening on port ' + PORT)
 })
